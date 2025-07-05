e99e9c90616cc5242c0c4ba989b08a9dbd5fb498
Irina <irinarakoto2005@gmail.com>
2025-07-05 18:53:30 +0300

    feat: Debouche

--- src/components/Debouche.jsx
+++ src/components/Debouche.jsx
@@ -4,13 +4,13 @@ import { Container } from "./Container"
 export const Debouche = () => {
 return (
    <Container className={`py-6`}>
-        <div className="flex flex-row gap-[40px] items-center">
+        <div className="flex flex-row gap-10 items-center">
             <div className="flex flex-col gap-5 flex-1">
-                <h2 className="text-5xl font-['EB-Garamond'] font-bold text-primary ">Quelques débouchés</h2>
-                <p className="text-primary text-xl">
+                <h2 className="text-5xl title font-semibold text-primary ">Quelques débouchés</h2>
+                <p className="text-primary text-xl font-[Helvetica]">
                     Formez-vous à une panoplie de métiers du numérique avec nous et venez acquérir les fondements de la programmation (qui vous servira pour les différents métiers) ! Ce sont des métiers porteurs, des métiers du présent, des métiers du futur…
                 </p>
-                <p className="text-primary text-xl">
+                <p className="text-primary text-xl font-[Helvetica]">
                     Développeur back end, développeur front end, développeurs mobile, chefs de projet, exploitants cloud, analystes cybersécurité, pentesters, auditeur technique SSI, auditeur organisationnel SSI, administrateur système et réseau, développeur de sécurité…
                 </p>
             </div>
