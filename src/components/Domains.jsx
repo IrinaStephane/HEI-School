414ef9c07af8ebe09eccae50a07665fec9f73d66
Irina <irinarakoto2005@gmail.com>
2025-07-05 18:53:49 +0300

    feat: DOmains

--- src/components/Domains.jsx
+++ src/components/Domains.jsx
@@ -6,10 +6,10 @@ const Domains = () => {
     return (
         <div className="bg-[#F1F1F1] py-16">
             <Container>
-                <h2 className="text-center text-4xl font-bold text-primary mb-4 font-['EB-Garamond']">
+                <h2 className="text-center text-4xl font-bold text-primary mb-4 title">
                     Domaines
                 </h2>
-                <p className="text-center text-primary mb-12">
+                <p className="text-center text-primary mb-12 font-[Helvetica]">
                     Au cours de vos trois ans de formation, naviguez entre les domaines les plus porteurs du numérique du présent et de l’avenir :
                 </p>
 
@@ -17,17 +17,17 @@ const Domains = () => {
                     {domains.map((domain, index) => (
                         <div
                             key={index}
-                            className="bg-white rounded-3xl p-8 flex flex-col items-center text-center shadow hover:shadow-lg transition-shadow duration-300 w-[360px] min-h-[525px]"
+                            className="bg-white rounded-3xl p-8 flex flex-col items-center text-center shadow hover:shadow-lg transition-shadow duration-300 w-90 min-h-131"
                         >
                             <img
                                 src={domain.icon}
                                 alt={domain.title}
                                 className="w-40 h-40 object-contain mb-5"
                             />
-                            <h3 className="text-2xl font-semibold text-secondary mb-4 font-['EB-Garamond']">
+                            <h3 className="text-2xl font-semibold text-secondary mb-4 title">
                                 {domain.title}
                             </h3>
-                            <p className="text-primary text-base ">
+                            <p className="text-primary text-base font-[Helvetica]">
                                 {domain.description}
                             </p>
                         </div>
